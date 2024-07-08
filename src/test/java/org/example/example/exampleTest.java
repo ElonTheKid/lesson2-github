88e9d93b25b29980e0c339e36c34337c03c4177e
ElonTheKid <ilya.belous.2033@mail.ru>
2024-07-08 23:50:19 +0200

    first init from console

---
+++ src/test/java/org/example/example/exampleTest.java
@@ -0,0 +1,12 @@
+package org.example.example;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class exampleTest {
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(  256 > 100 );
+    }
+}
