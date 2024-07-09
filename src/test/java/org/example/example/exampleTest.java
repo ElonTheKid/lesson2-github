71656ae37612b1409e263998e7630227e30dc7c5
ElonTheKid <ilya.belous.2033@mail.ru>
2024-07-09 23:52:57 +0200

    bigger or equell

--- src/test/java/org/example/example/exampleTest.java
+++ src/test/java/org/example/example/exampleTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 public class exampleTest {
     @Test
     void exampleTest0(){
-        Assertions.assertTrue(  2222 > 1111 );
+        Assertions.assertTrue(  2222 >= 1111 );
     }
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(  3333 > 2222 );
+        Assertions.assertTrue(  3333 >= 2222 );
     }
 }
