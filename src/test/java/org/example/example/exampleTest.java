d656954126c2035d3d769b01afccb13856dc3808
ElonTheKid <ilya.belous.2033@mail.ru>
2024-07-09 23:33:49 +0200

    add second test

--- src/test/java/org/example/example/exampleTest.java
+++ src/test/java/org/example/example/exampleTest.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.Test;
 
 public class exampleTest {
     @Test
-    void exampleTest(){
-        Assertions.assertTrue(  2998 > 100 );
+    void exampleTest0(){
+        Assertions.assertTrue(  2222 > 1111 );
+    }
+    @Test
+    void exampleTest1(){
+        Assertions.assertTrue(  3333 > 2222 );
     }
 }
